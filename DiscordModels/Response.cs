2d922ba83580e5c7186f6d11cf3013e8b9c24e50
Glatrix <73367967+Glatrix@users.noreply.github.com>
2023-02-15 07:45:01 -0600

    Typo

---
+++ DiscordModels/Response.cs
@@ -0,0 +1,24 @@
+﻿using DiscordAPIModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordAPIModels
+{
+    public class Response
+    {
+        public ResponseType type { get; set; }
+        public ResponseData? data { get; set; }
+    }
+    public class ResponseData
+    {
+        public bool tts { get; set; }
+        public string? content { get; set; }
+        public List<Embed>? embeds { get; set; }
+        public MessageFlags? flags { get; set; }
+        public List<Attatchment>? attachments { get; set; }
+    }
+}
